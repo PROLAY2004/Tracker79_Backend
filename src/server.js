e9ead8d1cc66e665af2d25b465902ea03b2c2bb4
Prolay Halder <prolayhalder2004@gmail.com>
2026-02-27 11:14:17 +0530

    Fix : server.js bug fix

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import dashboardRoutes from './routes/dashboardRoutes.js';
 import connectDB from './config/dbConfig.js';
 import TokenValidation from './validations/middleware/TokenValidation.js';
 
-
+await connectDB();
 
 const app = express();
 const tokenValidator = new TokenValidation();
@@ -16,18 +16,10 @@ const tokenValidator = new TokenValidation();
 app.use(cors(configuration.CORS));
 app.use(express.json());
 
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
-
 app.use('/', (req,res)=> {
   res.send('Backend Ok')
 })
+
 app.use('/user/auth', authRoutes);
 app.use('/user/account', tokenValidator.accessTokenValidator, dashboardRoutes);
 
