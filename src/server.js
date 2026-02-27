2ad66dfa8f440ff17bf15e848479d8cf0185ffad
Prolay Halder <prolayhalder2004@gmail.com>
2026-02-27 10:57:51 +0530

    Feat : vercel deployment configuration

--- src/server.js
+++ src/server.js
@@ -21,6 +21,6 @@ app.use('/user/account', tokenValidator.accessTokenValidator, dashboardRoutes);
 
 app.use(errorHandler);
 
-app.listen(configuration.PORT, () => {
-  console.log(`Tracker79 listening on port ${configuration.PORT}`);
-});
+// app.listen(configuration.PORT, () => {
+//   console.log(`Tracker79 listening on port ${configuration.PORT}`);
+// });
